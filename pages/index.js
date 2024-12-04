15612a72f7513f141b0fd77df438cc0fe02adbd3
Wendhel Alves <108488787+Wendhel06@users.noreply.github.com>
2024-12-04 14:06:47 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Te amo jojoice, se você me ama, dá uma risadinha !!!!</h1>
+    return <h1>Te amo jojoice, se você me ama, dá uma risadinha! =)</h1>
 }
 
 export default Home;
