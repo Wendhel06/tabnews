62b758752867ade2bd7b1b58e980055282e5e86f
Wendhel06 <wendhelalves7@gmail.com>
2025-02-22 13:19:11 -0300

    chore: fix description in ´describe method´ and `test method`

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -5,8 +5,8 @@ beforeAll(async () => {
 });
 
 describe("GET /api/v1/status", () => {
-  describe("Retrieving current system status", () => {
-    test("Running pending migrations", async () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
       const response = await fetch("http://localhost:3000/api/v1/status");
       expect(response.status).toBe(200);
 
